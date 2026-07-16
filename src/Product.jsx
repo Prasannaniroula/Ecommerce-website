20d10d78f098819543bbc0b35dbff8f17cd7706e
Prasannaniroula <Prasannaniroula5@gmail.com>
2026-07-16 22:54:52 +0545

    refactor styles and update product component for improved layout and responsiveness

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,36 +1,36 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import AddToCart from './AddToCart'
-import { addItem, removeItem, clearCart } from './RTK/slice'
-import {fetchProducts} from './RTK/productSlice'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import AddToCart from "./AddToCart";
+import { addItem, removeItem, clearCart } from "./RTK/slice";
+import { fetchProducts } from "./RTK/productSlice";
 
 export default function Product() {
-    const dispatch = useDispatch()
-    useEffect(()=>{
-        dispatch(fetchProducts())
-    },[])
-    const productlist = useSelector((state)=> state.products.items)
-    console.log(productlist)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+  const productlist = useSelector((state) => state.products.items);
+  console.log(productlist);
   return (
-   <>
-    <div className='grid'>
-        {
-            productlist.length && productlist.map((item)=>(
-                <div className='card' key={item.id}>
-                    <img src={item.thumbnail} alt='product-image'/>
-                    <div className='content'>
-                        <div className='title'>{item.title}</div>
-                        <div className='description'>{item.description}</div>
-                        <div className='category'>{item.category}</div>
-                        <div className='Price'>{item.price}</div>
-                        <div className='Available'>{item.availabilityStatus}</div>
-                    </div>
-                </div>
-            ))
-        }
-
-
-    </div>
-   </>
-  )
+    <>
+      <div className="grid">
+        {productlist.length &&
+          productlist.map((item) => (
+            <div className="card" key={item.id}>
+              <img src={item.thumbnail} alt="product-image" />
+              <div className="content">
+                <div className="title">{item.title}</div>
+                <div className="description">{item.description}</div>
+                <div className="brand">{item.brand}</div>
+                <div className="Price">${item.price}</div>
+                <div className="Available">{item.availabilityStatus}</div>
+              </div>
+              <button onClick={() => dispatch(addItem(item))}>
+                Add to Cart
+              </button>
+            </div>
+          ))}
+      </div>
+    </>
+  );
 }
